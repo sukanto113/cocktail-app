fb466fa59bfbf64877b7ec046aae18d1f986e24a
Sukanto Saha <sukanto113@gmail.com>
2024-06-25 19:52:07 +0600

    add profile update

--- src/main/java/com/cocktail/app/controller/UserController.java
+++ src/main/java/com/cocktail/app/controller/UserController.java
@@ -38,6 +38,25 @@ public class UserController {
         }
     }
 
+    @PostMapping
+    ResponseEntity<ProfileInfo> getUser(@RequestBody ProfileInfo formFields, @CurrentSecurityContext(expression = "authentication.principal") Jwt jwt) {
+        Long userId = Long.parseLong(jwt.getSubject());
+        Optional<UserInfo> userInfoOptional = userRepository.findById(userId);
+        if (userInfoOptional.isPresent()) {
+            UserInfo user = userInfoOptional.get();
+            user.setFullName(formFields.fullName());
+            user.setPhone(formFields.phone());
+            user.setPicture(formFields.picture());
+            user.setStreet(formFields.street());
+            user.setCity(formFields.city());
+            user.setCountry(formFields.country());
+            userRepository.save(user);
+            return ResponseEntity.ok(new ProfileInfo(user.getFullName(), user.getEmail(), user.getPhone(), user.getPicture(), user.getStreet(), user.getCity(), user.getCountry()));
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
     @PostMapping("/register")
     ResponseEntity<RegistrationResponse> register(@RequestBody RegistrationForm formFields) {
         UserInfo newUser = new UserInfo();
