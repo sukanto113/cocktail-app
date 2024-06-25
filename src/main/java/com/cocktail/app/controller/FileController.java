5c1775776278d55765a70496ce19ce1fc44b91c3
Sukanto Saha <sukanto113@gmail.com>
2024-06-25 20:57:29 +0600

    upload profile image

--- src/main/java/com/cocktail/app/controller/FileController.java
+++ src/main/java/com/cocktail/app/controller/FileController.java
@@ -1,11 +1,15 @@
 package com.cocktail.app.controller;
 
+import com.cocktail.app.entity.UserInfo;
 import com.cocktail.app.model.FileUploadResponse;
+import com.cocktail.app.repository.UserRepository;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.CurrentSecurityContext;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -16,11 +20,18 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import java.nio.file.StandardCopyOption;
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/file")
 public class FileController {
+
+    private final UserRepository userRepository;
+
+    private FileController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @GetMapping("/image/{filename}")
     @ResponseBody
@@ -37,35 +48,45 @@ public class FileController {
         }
     }
 
-    @GetMapping("/upload/{filename}")
+    @GetMapping("/profile-picture/{filename}")
     @ResponseBody
     public ResponseEntity<Resource> serveFile(@PathVariable String filename) throws MalformedURLException {
-        String fileDir = Paths.get(System.getProperty("user.dir")).getParent().toString() + "/file/upload";
+        String fileDir = Paths.get(System.getProperty("user.dir")).getParent().toString() + "/file/profile-picture";
         Path rootLocation = Paths.get(fileDir);
         Path file = rootLocation.resolve(filename);
         System.out.println(file);
         Resource resource = new UrlResource(file.toUri());
         if (resource.exists() || resource.isReadable()) {
-            return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
-                    "attachment; filename=\"" + filename + "\"").body(resource);
+            return ResponseEntity.ok().contentType(MediaType.IMAGE_PNG).body(resource);
         } else {
             return ResponseEntity.notFound().build();
         }
     }
 
-    @PostMapping("/upload")
-    ResponseEntity<FileUploadResponse> upload(@RequestParam("file") MultipartFile file) throws IOException {
+    @PostMapping("/profile-picture")
+    ResponseEntity<FileUploadResponse> upload(@RequestParam("file") MultipartFile file, @CurrentSecurityContext(expression = "authentication.principal") Jwt jwt) throws IOException {
+        Long userId = Long.parseLong(jwt.getSubject());
+        Optional<UserInfo> userInfoOptional = userRepository.findById(userId);
+        if (!userInfoOptional.isPresent()) {
+            return ResponseEntity.ok(new FileUploadResponse(false, null, "User not found"));
+        }
+
+        UserInfo user = userInfoOptional.get();
+
         UUID uuid = UUID.randomUUID();
         String uuidAsString = uuid.toString();
         String uploadFileName = uuidAsString + "_" + file.getOriginalFilename();
         String fileDir = Paths.get(System.getProperty("user.dir")).getParent().toString() + "/file";
-        String uploadDir = fileDir + "/upload";
+        String uploadDir = fileDir + "/profile-picture";
         Path uploadDirPath = Paths.get(uploadDir);
         if (!Files.exists(uploadDirPath)) {
             Files.createDirectories(uploadDirPath);
         }
         Path uploadFilePath = Paths.get(uploadDir + "/" + uploadFileName);
         Files.copy(file.getInputStream(), uploadFilePath, StandardCopyOption.REPLACE_EXISTING);
-        return ResponseEntity.ok(new FileUploadResponse(true, "file/upload/" + uploadFileName, null));
+        String filePath = "file/profile-picture/" + uploadFileName;
+        user.setPicture(filePath);
+        userRepository.save(user);
+        return ResponseEntity.ok(new FileUploadResponse(true, filePath, null));
     }
 }
